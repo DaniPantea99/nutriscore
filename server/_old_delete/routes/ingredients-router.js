913413401c9b446521d3674ccfe61553c22c8bce
Daniel Pantea <dani.pantea@gmail.com>
2023-03-16 09:36:12 +0200

    update 16.03.23 - delete old unnecessary files

--- server/_old_delete/routes/ingredients-router.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const IngredientCtrl = require('../controllers/ingredients-ctrl');
-const router = express.Router();
-
-router.get('/', IngredientCtrl.getInfoConsIngredients);
-
-module.exports = router;
